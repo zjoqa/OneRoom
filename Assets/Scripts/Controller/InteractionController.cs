1c37e9837e5f8d2393b01d55d6b8bea2919639f7
Chan <137601394+zjoqa@users.noreply.github.com>
2024-11-20 11:42:58 +0900

    ###Update.003

--- Assets/Scripts/Controller/InteractionController.cs
+++ Assets/Scripts/Controller/InteractionController.cs
@@ -11,35 +11,42 @@ using UnityEngine.UI;
 
     RaycastHit  hitInfo;
 
-    [SerializeField] GameObject go_NormalCrosshair;
-    [SerializeField] GameObject go_InteractiveCrosshair;
-    [SerializeField] GameObject go_Crosshair;
-    [SerializeField] GameObject go_Cursor;
-    [SerializeField] GameObject go_TargetNameBar;
-    [SerializeField] Text txt_TargetName;
+    [SerializeField] GameObject go_NormalCrosshair; // 일반 크로스헤어
+    [SerializeField] GameObject go_InteractiveCrosshair; // 상호작용 크로스헤어
+    [SerializeField] GameObject go_Crosshair; // 크로스헤어 (부모)
+    [SerializeField] GameObject go_Cursor; // Arrow
+    [SerializeField] GameObject go_TargetNameBar; // ToolTip
+    [SerializeField] Text txt_TargetName; // ToolTip 텍스트
 
     bool isContact = false; // 상호작용 가능 여부
     public static  bool isInteract = false; // 상호작용 중인지 아닌지
 
-    [SerializeField] ParticleSystem ps_QuestionEffect;
+    [SerializeField] ParticleSystem ps_QuestionEffect; // 물음표 투사체
 
-    DialogueManager theDM;
+    [SerializeField] Image img_Interaction; // 상호작용 마름모 이미지
+    [SerializeField] Image img_InteractionEffect; // 상호작용 이펙트
+
+    DialogueManager theDM; // 대화창 매니저
 
     public void HideUI() // 대화창 활성화 중일 때 UI 숨김
     {
         go_Crosshair.SetActive(false);
         go_Cursor.SetActive(false);
+        go_TargetNameBar.SetActive(false);
     }
 
     void Start()
     {
-        theDM = FindObjectOfType<DialogueManager>();
+        theDM = FindObjectOfType<DialogueManager>(); // 대화창 매니저 찾아오기
     }   
 
     void Update()
     {
-        CheckObject();
-        ClickLeftBtn();
+        if(!isInteract)
+        {
+            CheckObject(); 
+            ClickLeftBtn();
+        }
     }
 
     private void CheckObject()
@@ -58,16 +65,20 @@ using UnityEngine.UI;
     // Interaction Tag를 가진 객체에 hit가 될 때 실행
     private void Contact() // 상호작용 가능 여부 확인
     {
-        if(hitInfo.transform.CompareTag("Interaction"))
+        if(hitInfo.transform.CompareTag("Interaction")) // 충돌한 객체가 Interaction Tag를 가지고 있으면
         {
-            go_TargetNameBar.SetActive(true);
-            txt_TargetName.text = hitInfo.transform.GetComponent<InteractionType>().GetName();
+            go_TargetNameBar.SetActive(true); // ToolTip 활성화
+            txt_TargetName.text = hitInfo.transform.GetComponent<InteractionType>().GetName(); // ToolTip 텍스트 설정
             if(!isContact) // 상호작용이 불가능 할 때 (중복 실행 방지)
             {
                 // Interaction의 객체에 크로스헤어가 머물러 있으면 isContact가 ture이기 때문에 조건문이 맞지 않아 중복 실행 방지
                 isContact = true; 
                 go_InteractiveCrosshair.SetActive(true);
                 go_NormalCrosshair.SetActive(false);
+                StopCoroutine("Interaction");
+                StopCoroutine("InteractionEffect");
+                StartCoroutine("Interaction", true);
+                StartCoroutine("InteractionEffect");
             }
         }
         else{// 충돌 한 객체가 Interaction Tag가 아니면
@@ -83,8 +94,57 @@ using UnityEngine.UI;
             isContact = false;
             go_InteractiveCrosshair.SetActive(false);
             go_NormalCrosshair.SetActive(true);
+            StopCoroutine("Interaction");
+            StartCoroutine("Interaction", false);
         }
     }
+
+    private IEnumerator Interaction(bool p_Appear)
+    {
+        Color color = img_Interaction.color;
+        if(p_Appear)
+        {
+            color.a = 0;
+            while(color.a < 1)
+            {
+                color.a += 0.1f;
+                img_Interaction.color = color;
+                yield return null;
+            }
+        }
+        else
+        {
+            while(color.a > 0)
+            {
+                color.a -= 0.1f;
+                img_Interaction.color = color;
+                yield return null;
+            }
+        }
+    }
+
+    private IEnumerator InteractionEffect()
+    {
+        while(isContact && !isInteract)
+        {
+            Color color = img_InteractionEffect.color; // 투명도 변수 초기화
+            color.a = 0.5f; // 투명도 설정
+
+            img_InteractionEffect.transform.localScale = new Vector3(1,1,1); // 크기 초기화
+            Vector3 t_scale = img_InteractionEffect.transform.localScale; // 크기 변수 초기화
+
+            while(color.a > 0)
+            {
+                color.a -= 0.01f; // 투명도 감소
+                img_InteractionEffect.color = color; // 투명도 변경
+                t_scale.Set(t_scale.x + Time.deltaTime, t_scale.y + Time.deltaTime, t_scale.z + Time.deltaTime); // 크기 증가
+                img_InteractionEffect.transform.localScale = t_scale; // 크기 변경
+                yield return new WaitForSeconds(0.01f);
+            }
+            yield return null;
+        }
+    }
+
 
     private void ClickLeftBtn()
     {
@@ -103,6 +163,13 @@ using UnityEngine.UI;
     private void Interact() // 상호작용 실행
     {
         isInteract = true;
+
+        StopCoroutine("Interaction"); // 상호작용 중일 때 상호작용 이미지 중복 실행 방지
+        // InteractionEffect 은 isInteract가 상호작용시 true로 바뀌기 때문에 자동으로 중지됨
+        Color color = img_Interaction.color;
+        color.a = 0;
+        img_Interaction.color = color;
+
         Vector3 t_targetPos = hitInfo.transform.position;
         ps_QuestionEffect.gameObject.SetActive(true);
         ps_QuestionEffect.GetComponent<QuestionEffect>().SetTarget(t_targetPos);
--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -6,49 +6,49 @@ using UnityEngine;
     public class PlayerController : MonoBehaviour
     {
         [SerializeField]
-        private Transform tf_Crosshair;
+        private Transform tf_Crosshair; // 크로스헤어
         [SerializeField]
-        private Transform tf_Cam;
+        private Transform tf_Cam; // Main Camera
         [SerializeField]
         private Vector2 camBoundary; // 캠의 가두기 영역
         [SerializeField]
-        private float sightMoveSpeed; // 좌우 움직임 스피드
+        private float sightMoveSpeed; // 카메라 움직임 속도
         [SerializeField]
-        private float sightSensitivity; // 고개 움직임 속도
+        private float sightSensitivity; // 카메라 회전 속도
         [SerializeField]
-        private float lookLimitX;
+        private float lookLimitX; // 카메라 좌,우 방향 제한 영역
         [SerializeField]
-        private float lookLimitY;
+        private float lookLimitY; // 카메라 상,하 방향 제한 영역
 
         private float currentAngleX;
         private float currentAngleY;
 
-        // 카메라 제한 영역 표시 이미지
+        // 카메라 제한 UI
         [SerializeField] GameObject go_NotCamDown;
         [SerializeField] GameObject go_NotCamUp;
         [SerializeField] GameObject go_NotCamLeft;
         [SerializeField] GameObject go_NotCamRight;
 
         // 카메라 원래 위치
-        float originPosY; 
+        float originPosY;
 
         private void Start() {
             // 카메라 원래 위치 저장
             originPosY = tf_Cam.localPosition.y;
         }
-        // Update is called once per frame
+
         void Update()
         {
             // 커서 숨기기
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Confined;
-            if(!InteractionController.isInteract)
+            if(!InteractionController.isInteract) // 상호작용 중이 아니면
             {
                 CrosshairMoving(); // 크로스헤어 움직임 
                 ViewMoving(); // 화면 움직임(마우스)  
                 KeyViewMoving(); // 화면 움직임 (키보드)
                 CameraLimit();// 카메라 고정
-                NotCamUI();
+                NotCamUI(); // 카메라 제한 UI
             }
         }
         
@@ -59,14 +59,14 @@ using UnityEngine;
             go_NotCamLeft.SetActive(false);
             go_NotCamRight.SetActive(false);
             
-            if(currentAngleY >= lookLimitX) // 현재 카메라 오른쪽 방향 각도가 제한 영역 오른쪽 각도보다 크거나 같으면
+            if(currentAngleY >= lookLimitX) // 현재 카메라 Y축 방향(좌,우) 각도가 제한 영역 오른쪽 각도보다 크거나 같으면
                 go_NotCamRight.SetActive(true);
-            else if(currentAngleY <= -lookLimitX) // 현재 카메라 오른쪽 방향 각도가 제한 영역 왼쪽 각도보다 작거나 같으면
+            else if(currentAngleY <= -lookLimitX) // 현재 카메라 Y축 방향(좌,우) 각도가 제한 영역 왼쪽 각도보다 작거나 같으면
                 go_NotCamLeft.SetActive(true);
             
-            if(currentAngleX >= lookLimitY) // 현재 카메라 아래 방향 각도가 제한 영역 아래 각도보다 크거나 같으면
+            if(currentAngleX >= lookLimitY) // 현재 카메라 X축 방향(상,하) 각도가 제한 영역 아래 각도보다 크거나 같으면
                 go_NotCamDown.SetActive(true);
-            else if(currentAngleX <= -lookLimitY) // 현재 카메라 아래 방향 각도가 제한 영역 위 각도보다 작거나 같으면   
+            else if(currentAngleX <= -lookLimitY) // 현재 카메라 X축 방향(상,하) 각도가 제한 영역 위 각도보다 작거나 같으면   
                 go_NotCamUp.SetActive(true);
         }
 
@@ -96,18 +96,19 @@ using UnityEngine;
         {
             if(Input.GetAxisRaw("Horizontal") != 0) // 좌우 키 입력이 있으면
             {
-                // 현재 카메라 오른쪽 방향 각도에 좌우 키 입력 값 * 고개 움직임 속도를 더함
+                // 현재 카메라 좌,우 방향 각도에 좌우 키 입력 값 * 고개 움직임 속도를 더함
                 currentAngleY += sightSensitivity * Input.GetAxisRaw("Horizontal");
-                // 현재 카메라 오른쪽 방향 각도를 제한 영역 왼쪽 각도와 오른쪽 각도 사이로 제한
+                // 현재 카메라 좌,우 방향 각도를 제한 영역 왼쪽 각도와 오른쪽 각도 사이로 제한
                 currentAngleY = Mathf.Clamp(currentAngleY , -lookLimitX , lookLimitX);
-                // 카메라의 x축 위치에 좌우 키 입력 값 * 좌우 움직임 스피드를 더함
+                // 카메라의 포지션에 좌우 키 입력 값 * 좌우 움직임 스피드를 더함
                 tf_Cam.localPosition = new Vector3(tf_Cam.localPosition.x + sightMoveSpeed * Input.GetAxisRaw("Horizontal"), tf_Cam.localPosition.y, tf_Cam.localPosition.z);
             }
-            if(Input.GetAxisRaw("Vertical") != 0)
+            if(Input.GetAxisRaw("Vertical") != 0) // 상,하 키 입력이 있으면
             {
-                currentAngleX += sightSensitivity * -Input.GetAxisRaw("Vertical");
-                currentAngleX = Mathf.Clamp(currentAngleX , -lookLimitY, lookLimitY);
-                tf_Cam.localPosition = new Vector3(tf_Cam.localPosition.x, tf_Cam.localPosition.y + sightMoveSpeed * Input.GetAxisRaw("Vertical"), tf_Cam.localPosition.z);
+                currentAngleX += sightSensitivity * -Input.GetAxisRaw("Vertical"); // 현재 카메라 상,하에 회전 민감도 * 상,하 키 입력 (상,하 반전)
+                currentAngleX = Mathf.Clamp(currentAngleX , -lookLimitY, lookLimitY); // 현재 카메라 상,하 각도를 제한
+                // 카메라 이동 구현 (카메라 포지션 Y축 + 상,하 키 입력 * 카메라 움직임 속도)
+                tf_Cam.localPosition = new Vector3(tf_Cam.localPosition.x, tf_Cam.localPosition.y + sightMoveSpeed * Input.GetAxisRaw("Vertical"), tf_Cam.localPosition.z); 
             }
         }
 
@@ -115,14 +116,15 @@ using UnityEngine;
         {   // 크로스헤어의 x 포지션이 화면 오른쪽 끝 - 100 의 값보다 크거나 || 왼쪽 끝 + 100 보다 작으면
             if (tf_Crosshair.localPosition.x > (Screen.width / 2 - 100) || tf_Crosshair.localPosition.x < (-Screen.width / 2 + 100))
             {
+                // currentAngleY에 크로스헤어 x 포지션이 0보다 크면 카메라 좌,우 회전 속도를 더하고 아니면 빼줌
                 currentAngleY += (tf_Crosshair.localPosition.x > 0) ? sightSensitivity : -sightSensitivity;
-                currentAngleY = Mathf.Clamp(currentAngleY , -lookLimitX , lookLimitX);
-                // 왼쪽으로 움직이면 sightMovespeed 빼줌 오른쪽은 더해줌
+                currentAngleY = Mathf.Clamp(currentAngleY , -lookLimitX , lookLimitX); // 현재 카메라 좌,우 각도를 제한
+                // 왼쪽으로 움직이면 카메라 움직임 속도를 빼주고 오른쪽으로 움직이면 더해줌
                 float t_applySpeed = (tf_Crosshair.localPosition.x > 0) ? sightMoveSpeed : -sightMoveSpeed; 
                 // 카메라의 로컬포지션에 t_applySpeed의 값을 더함
                 tf_Cam.localPosition =  new Vector3(tf_Cam.localPosition.x + t_applySpeed, tf_Cam.localPosition.y, tf_Cam.localPosition.z);
             }
-            if (tf_Crosshair.localPosition.y > (Screen.height / 2 - 100) || tf_Crosshair.localPosition.y < (-Screen.height / 2 + 100)) 
+            if (tf_Crosshair.localPosition.y > (Screen.height / 2 - 100) || tf_Crosshair.localPosition.y < (-Screen.height / 2 + 100))
             {
                 currentAngleX += (tf_Crosshair.localPosition.y > 0) ? -sightSensitivity : sightSensitivity;
                 currentAngleX = Mathf.Clamp(currentAngleX , -lookLimitY , lookLimitY);
